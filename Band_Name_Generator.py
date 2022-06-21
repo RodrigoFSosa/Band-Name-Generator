28a4467d67df5e762b4ec448cfa2842d369c142c
Rodrigo Sosa González <105399844+RodrigoFSosa@users.noreply.github.com>
2022-06-21 15:02:51 -0500

    Add files via upload

---
+++ Band_Name_Generator.py
@@ -0,0 +1,9 @@
+# Saludo del programa
+print("Bienvenido al generado de nombres de banda.")
+# Se pide al usuario la ciudad en la que creció
+ciudad = input("¿En qué ciudad creciste?\n")
+# Se pide al usuario el nombre de su mascota
+mascota = input("¿Cuál es el nombre de tu mascota?\n")
+# Se combina el nombre de la ciudad con el de su mascota y muestra el nombre de la banda
+print("El nombre de tu banda puede ser " + ciudad + " " + mascota)
+
